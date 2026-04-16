1972be853892eac16fdae75e8c5d5ada02133a0e
mohamedamrnagy35-spec <mohamedamrnagy35@gmail.com>
2026-04-17 00:16:29 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,22 +3,14 @@ from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
-# This line is the "Key" to fixing the Bridge error
-CORS(app, resources={r"/*": {"origins": "*"}})
-
-@app.route('/')
-def home():
-    return "SAVAGE ENGINE ONLINE"
+CORS(app, resources={r"/*": {"origins": "*"}}) # This is the "Magic" line
 
 @app.route('/api/check', methods=['POST', 'OPTIONS'])
 def check():
-    if request.method == 'OPTIONS':
-        return '', 200
-    
+    if request.method == 'OPTIONS': return '', 200
     data = request.get_json()
     name = data.get("username")
     url = f"https://auth.roblox.com/v1/usernames/validate?username={name}&birthday=2004-10-10"
-    
     try:
         r = requests.get(url, timeout=5)
         return jsonify(r.json())
